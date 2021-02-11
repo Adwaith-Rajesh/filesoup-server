c207a7091a01d2205b8fa4ac0baef670e3b4f980
Adwaith-Rajesh <adwaithrajesh3180@gmail.com>
2021-02-11 20:50:55 +0530

    😁A�ded decorator to authorize | cleaned up some stuff

--- filesoup_server/server/app.py
+++ filesoup_server/server/app.py
@@ -1,52 +1,108 @@
 import os
+from requests.api import post
 
+import werkzeug
 from flask import Flask, send_file, send_from_directory, abort
 from flask_restful import Resource, Api, reqparse
 
+from filesoup_server.files.file_manager import check_file_exists
 from .server_utils import ServerData
 from .server_utils import verify_user
 from .server_utils import load_index
+from .server_utils import dec_verify_user
+from .server_utils import check_file_exists_on_index_data
 
 app = Flask(__name__)
 api = Api(app)
 
 file_index_data = {}
 
+
+# normal authentication parser
 auth_parser = reqparse.RequestParser()
-auth_parser.add_argument("username", type=str, required=True, help="username is requires for authentication..")
-auth_parser.add_argument("password", type=str, required=True, help="password is required with username for authentication..")
+auth_parser.add_argument(
+    "username",
+    type=str,
+    required=True,
+    help="username is required for authentication..",
+)
+auth_parser.add_argument(
+    "password",
+    type=str,
+    required=True,
+    help="password is required with username for authentication..",
+)
+
+# post requests parser
+post_parser = reqparse.RequestParser()
+post_parser.add_argument(
+    "file",
+    type=werkzeug.datastructures.FileStorage,
+    required=True,
+    help="The type of the file to be written is requires...",
+    location="files",
+)
+
 
 class Indexdata(Resource):
     """Deals with managing the index data that has been generated"""
-    
 
     def get(self):
-        """return the entire index data to the client"""
-        auth = auth_parser.parse_args()
-        if verify_user(username=auth["username"], password=auth["password"]):
+        @dec_verify_user(auth_parser.parse_args())
+        def in_get():
             return send_from_directory(os.getcwd(), "file-index.json")
-        else:
-            abort(401, description="invalid password or username")
+
+        return in_get()
 
 
 class Files(Resource):
     """Allows the client to get the files bases on the id"""
 
     def get(self, _type: str, _id: str):
-        if _type in file_index_data:
-            if _id in file_index_data[_type]:
-                print('resturning stuff')
-                return send_from_directory(file_index_data[_type][_id]["path"], file_index_data[_type][_id]["name"])
+        @dec_verify_user(auth_parser.parse_args())
+        def in_get():
+            cv = check_file_exists_on_index_data(
+                data=file_index_data, _type=_type, _id=_id
+            )
+            if cv:
+                return send_from_directory(
+                    file_index_data[_type][_id]["path"],
+                    file_index_data[_type][_id]["name"],
+                )
 
-            else:
+            elif cv == 2:
+                abort(404, descripton="The specified type of file cannot be found.")
+
+            elif cv == 3:
                 abort(404, description="The video id does not exists..")
 
-        else:
-            abort(404, descripton="The specified type of file cannot be found.")
+        return in_get()
+
+    def delete(self, _type: str, _id: str):
+        @dec_verify_user(auth_parser.parse_args())
+        def in_delete():
+            cv = check_file_exists_on_index_data(
+                data=file_index_data, _type=_type, _id=_id
+            )
+            if cv:
+                # TODO: remove the id from the index data, delete the file from the file system
+                pass
+            elif cv == 2:
+                pass
+
+            elif cv == 3:
+                pass
+
+    def post(self, _type: str, _id: str):
+        auth = auth_parser.parse_args()
+        post_args = post_parser.parse_args()
+        _file = post_args["file"]
+        _file.save("return_data.mp4")
 
 
 api.add_resource(Indexdata, "/indexdata")
 api.add_resource(Files, "/files/<string:_type>/<string:_id>")
+
 
 def start_server():
     global file_index_data
--- filesoup_server/server/server_utils.py
+++ filesoup_server/server/server_utils.py
@@ -8,11 +8,14 @@ from typing import Dict
 import json
 
 
+from flask import abort
+
 FileIndexData = Dict[str, Dict[str, str]]
 
 
 config = ConfigParser()
 config.read("./fs_config.cfg")
+
 
 @dataclass(frozen=True)
 class ServerData:
@@ -20,10 +23,12 @@ class ServerData:
     ip: str = config["server"]["ip"]
     port: int = config["server"]["port"]
     username: str = config["login"]["username"]
-    password: str = sha256(bytes(config["login"]["password"], encoding="utf8")).hexdigest()
+    password: str = sha256(
+        bytes(config["login"]["password"], encoding="utf8")
+    ).hexdigest()
 
 
-def load_index()-> FileIndexData:
+def load_index() -> FileIndexData:
     with open("file-index.json") as f:
         data = json.load(f)
 
@@ -33,5 +38,38 @@ def load_index()-> FileIndexData:
 def verify_user(username: str, password: str) -> bool:
     server_data = ServerData()
     pass_hash = sha256(bytes(password, encoding="utf8")).hexdigest()
-    if username == server_data.username and pass_hash == server_data.password: return True
+    if username == server_data.username and pass_hash == server_data.password:
+        return True
     return False
+
+
+def check_file_exists_on_index_data(data: FileIndexData, _type: str, _id: str) -> int:
+    # returns 1 if everything exists
+    # returns 2 if _type does not exists
+    # returns 3 if _id does not exists
+    if _type in data:
+        if _id in data[_type]:
+            return 1
+
+        else:
+            return 2
+
+    else:
+        return 3
+
+
+def dec_verify_user(user_args: Dict[str, str]):
+    def in_func(f):
+        def wrap(*args, **kwargs):
+            if verify_user(
+                username=user_args["username"], password=user_args["password"]
+            ):
+                rv = f(*args, **kwargs)
+                return rv
+
+            else:
+                abort(404, description="Invalid password or username")
+
+        return wrap
+
+    return in_func
